3a9e3b6e111ee4ca611a67239d3eee49de561aaf
shamitomita <shamitomita@gmail.com>
2023-01-18 19:53:21 -0500

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Shami Tomita on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
